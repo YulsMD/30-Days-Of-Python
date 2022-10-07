bc5686b2f31e2f0035fbbef8bb11c23da2e40466
Yulissa Martínez <yulissa.mtzd@gmail.com>
2022-10-07 15:38:49 -0500

    Print data types

---
+++ 01_Day/01_Day.py
@@ -0,0 +1,33 @@
+# Introduction
+# Day 1 - 30DaysOfPython Challenge
+
+# Excercise level: 2
+# 1.Operations
+print(3 + 4)   # addition(+)
+print(3 - 4)   # subtraction(-)
+print(3 * 4)   # multiplication(*)
+print(3 / 4)   # division(/)
+print(3 ** 4)  # exponential(**)
+print(3 % 4)   # modulus(%)
+print(3 // 4)  # Floor division operator(//)
+
+# 2.Strings on Python
+print('Yulissa')
+print('Martínez')
+print('Mexico')
+print('Im enjoying 30 days of python')
+
+# 3.Checking data types
+
+print(type(10))                  # Int
+print(type(9.8))                  
+print(type(3.14))                # Float
+print(type(1 + 3j))              # Complex
+print(type('Asabeneh'))          # String
+print(type([1, 2, 3]))           # List
+print(type({'name':'Asabeneh'})) # Dictionary
+print(type({9.8, 3.14, 2.7}))    # Set
+print(type((9.8, 3.14, 2.7)))    # Tuple
+
+
+# Excercise: level 3
